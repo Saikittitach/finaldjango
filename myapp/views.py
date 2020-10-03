35a440e7e650d1cf2a508773c65b631ab3ee53c2
Kittitach <saikittitach@hotmail.com>
2020-10-03 23:38:17 +0700

    auto refresh page

--- myapp/views.py
+++ myapp/views.py
@@ -11,24 +11,6 @@ def index(request):
 def datatable(request):
     # products  = Product.objects.all()
     form = Datatabase.objects.all()
-    # if request.method=="POST":
-        
-    #     firstname= request.POST['firstname']
-    #     lastname= request.POST['lastname']
-    #     email= request.POST['email']
-    #     phone= request.POST['phone']
-    #     desc= request.POST['desc']
-    #     print(firstname, lastname, email, phone,desc)
-    # return render(request, 'frontend/datatable.html',{'database': database}) 
-
-    # form = databaseform(request.POST or None)
-    # if form.is_valid():
-        
-    #     form.save()
-        
-    # context={'form':form}
-
-    # return render(request, 'frontend/datatable.html',context)
 
     if request.method=="POST":
     
@@ -37,21 +19,6 @@ def datatable(request):
             filled_form.save()
         form = databaseform()
     return render(request, 'frontend/datatable.html',{'form':form})
-
-
-    # form = databaseform(request.POST or None)
-    # context = {'form':form}
-    # if form.is_valid():
-    #     firstname = form.cleaned_data['firstname']
-    #     context.update({'firstname':firstname})
-    #     lastname = form.cleaned_data['lastname']
-    #     context.update({'lastname':lastname})
-    #     email = form.cleaned_data['email']
-    #     context.update({'email':email})
-    #     desc = form.cleaned_data['desc']
-    #     context.update({'desc':desc})
-
-    # return render(request,'frontend/datatable.html' , context)
 
 
 def contact(request):
